f8e186d120e4465753d866df70307a839c9ba5df
Alfonso Irarrazaval <alfonso@tether.education>
2025-06-04 16:58:51 +0200

    fix: imports

--- apps/app-1/src/App.vue
+++ apps/app-1/src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup lang="ts">
-import ComponentA from './components/ComponentA.vue';
+import ComponentA from '@/components/ComponentA.vue';
 import ComponentB from './components/ComponentB.vue';
 
 </script>
